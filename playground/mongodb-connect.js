a9cef7338afd8f13b0a6b07bf1637963d394acb9
Mohammed Farag <e.mohammedfarag@gmail.com>
2017-02-15 14:57:30 +0200

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+// const MongoClient=require('mongodb').MongoClient;
+// const ObjectID=require('mongodb').ObjectID;
+// destructure object, below code is idintical to above code.
+const {MongoClient,ObjectID}=require('mongodb');
+
+// var obj=new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').insertOne({
+  //   text:'Something to do',
+  //   completed: false
+  // },
+  // (err,result)=>{
+  //   if(err){
+  //     return console.log('Unable to insert Todo.',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  // });
+  // db.collection('Users').insertOne({
+  //   name:'Fego',
+  //   age: 29,
+  //   location: 'Cairo'
+  // },
+  // (err,result)=>{
+  //   if(err){
+  //     return console.log('Unable to insert User.',err);
+  //   }
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  // });
+
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,28 @@
+const {MongoClient,ObjectID}=require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+//fetch all todos collection. find returns mongodb cursor.
+// returned cursor has many functions such as toArray which return promise
+  // db.collection('Todos').find({completed:false}).toArray().then((docs)=>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },
+  // (err)=>{
+  //   console.log('Unable to fetch Todos.',err);
+  // });
+
+  db.collection('Todos').find().count().then((count)=>{
+    console.log('Todos count: ',count);
+  },
+  (err)=>{
+    console.log('Unable to fetch Todos.',err);
+  });
+
+  //db.close();
+});
