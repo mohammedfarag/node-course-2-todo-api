2beb18b005497b77ce13b9558ade514993818edf
Mohammed Farag <e.mohammedfarag@gmail.com>
2017-02-16 10:09:17 +0200

    Set up to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 // tell mongoose which promise lib would be used.
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose}=require('./db/mongoose')
 var {Todo}=require('./models/todo')
 var {User}=require('./models/user')
 
-var port=process.env.port || 3000;
+var port=process.env.PORT || 3000;
 
 var app=express();
 
