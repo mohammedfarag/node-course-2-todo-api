6c711f0ee522c322795fc9eb9cecbdaea927df49
Mohammed Farag <e.mohammedfarag@gmail.com>
2017-02-16 19:24:11 +0200

    add hashing and sample of JWT

---
+++ playground/hashing.js
@@ -0,0 +1,40 @@
+const {SHA256}=require('crypto-js');
+const jwt=require('jsonwebtoken');
+
+var data={id:10};
+
+var token=jwt.sign(data,'123abc');
+console.log(`token ${token}`);
+
+var decoded=jwt.verify(token,'123abc');
+console.log('decoded',decoded);
+
+//code below is a semulation for JWT (that exactly what JWT do create token and verify it)
+/*
+var message='I am user number 3';
+var hash=SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
+
+var data={id:4};
+var token={
+  data,
+  hash:SHA256(JSON.stringify(data)+'somesecret').toString()
+};
+
+//salt is add a value to hashed object to help identifying if data changed or not
+//salt value 'somesecret' is on server so if pirate user changed the data value
+// and tried to hash it again sure it will be equale the hashed data with salt on server.
+var resultHash=SHA256(JSON.stringify(token.data)+'somesecret').toString();
+
+// token.data.id=5;
+// token.hash=SHA256(JSON.stringify(token.data)).toString();
+
+
+if(resultHash===token.hash){
+  console.log('Data was not changed');
+}else {
+  console.log('Data was changed don\'t trust!');
+}
+*/
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,31 @@
 const mongoose=require('mongoose');
+const validator=require('validator');
 var User=mongoose.model('User',{
   email:{
     type:String,
     required:true,
     trim:true,
-    minlength:5}
+    minlength:5,
+    unique:true,
+    validate:{
+      validator:validator.isEmail,
+      message:'{VALUE} is not a vlid email.'
+    }
+  },
+  password:{
+    type:String,
+    require:true,
+    minlength:6
+  },
+  tokens:[{
+    access:{
+      type:String,
+      require:true
+    },
+    token:{
+      type:String,
+      require:true
+    }
+  }]
 });
 module.exports = {User};
