c007ae2ec2f5a1da8bbf9fc0ab2950a104635c48
Mohammed Farag <e.mohammedfarag@gmail.com>
2017-02-18 20:55:25 +0200

    Add POST users login route

--- server/models/user.js
+++ server/models/user.js
@@ -61,6 +61,18 @@ UserSchema.statics.findByToken=function(token){
   });
   return yup;
 };
+UserSchema.statics.findByCredentials=function(email,password){
+  var User=this;
+  return User.findOne({email}).then((user)=>{
+    if(!user) return Promise.reject();
+    return new Promise((resolve,reject)=>{
+      bcrypt.compare(password,user.password,(err,res)=>{
+        if(res) resolve(user);
+        else reject();
+      });
+    });
+  });
+};
 UserSchema.pre('save',function(next){
   var user=this;
   //takes modified properties and return true or false if property modified
